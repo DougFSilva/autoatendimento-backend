f7c1aa4bf3dd42343b5d9e0274c14abb9bfa4393
DougFSilva <doug.ferreira.dfs@gmail.com>
2022-11-23 13:30:32 -0300

    Iniciado o projeto com as dependências necessárias, menos a Spring security

---
+++ src/test/java/br/com/totemAutoatendimento/TotemAutoatendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.totemAutoatendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotemAutoatendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
