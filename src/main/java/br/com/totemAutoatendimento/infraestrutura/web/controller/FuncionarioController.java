7496bf7969712156ca1ae1e857c3c86eb6231bb2
DougFSilva <doug.ferreira.dfs@gmail.com>
2023-01-31 15:47:31 -0300

    Criada classe Funcionario e classe Totem, onde ambas tem um atributo por composição chamado Usuario, a fim de manter as classes mais coesas. Com isso, foram adicionados os usecases e toda implementação na infraestrutura para suportar essa modificação

---
+++ src/main/java/br/com/totemAutoatendimento/infraestrutura/web/controller/FuncionarioController.java
@@ -0,0 +1,124 @@
+package br.com.totemAutoatendimento.infraestrutura.web.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.totemAutoatendimento.aplicacao.funcionario.BuscaDadosDeFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.CriaFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.EditaFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.RemoveFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.dto.DadosCriarFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.dto.DadosDeFuncionario;
+import br.com.totemAutoatendimento.aplicacao.funcionario.dto.DadosEditarFuncionario;
+import br.com.totemAutoatendimento.aplicacao.usuario.AlteraSenhaDeUsuario;
+import br.com.totemAutoatendimento.aplicacao.usuario.dto.DadosAlterarSenhaDeUsuario;
+import br.com.totemAutoatendimento.dominio.funcionario.Funcionario;
+import br.com.totemAutoatendimento.dominio.usuario.Usuario;
+import br.com.totemAutoatendimento.infraestrutura.seguranca.AutenticacaoDeUsuario;
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/funcionario")
+public class FuncionarioController {
+
+	@Autowired
+	private CriaFuncionario criaFuncionario;
+
+	@Autowired
+	private RemoveFuncionario removeFuncionario;
+
+	@Autowired
+	private EditaFuncionario editaFuncionario;
+
+	@Autowired
+	private BuscaDadosDeFuncionario buscaDadosDeFuncionario;
+
+	@Autowired
+	private AlteraSenhaDeUsuario alteraSenhaDeUsuario;
+
+	@Autowired
+	private AutenticacaoDeUsuario autenticacaoDeUsuario;
+
+	@PostMapping
+	@CacheEvict(value = "buscarTodosFuncionario", allEntries = true)
+	@Operation(summary = "Criar funcionário", description = "Cria um Funcionário no sistema")
+	public ResponseEntity<Funcionario> criarFuncionario(@Valid @RequestBody DadosCriarFuncionario dados) {
+		Funcionario funcionario = criaFuncionario.criar(dados, usuarioAutenticado());
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(funcionario.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping("/{id}")
+	@CacheEvict(value = "buscarTodosFuncionario", allEntries = true)
+	@Operation(summary = "Remover funcionário", description = "Remove um Funcionário cadastrado no sistema")
+	public ResponseEntity<Void> removerFuncionario(@PathVariable Long id) {
+		removeFuncionario.remover(id, usuarioAutenticado());
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	@CacheEvict(value = "buscarTodosFuncionario", allEntries = true)
+	@Operation(summary = "Editar funcionário", description = "Edita um Funcionário cadastrado no sistema")
+	public ResponseEntity<DadosDeFuncionario> editarFuncionario(@PathVariable Long id,
+			@RequestBody @Valid DadosEditarFuncionario dados) {
+		DadosDeFuncionario dadosDeFuncionario = editaFuncionario.editar(id, dados, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeFuncionario);
+	}
+	
+	@PatchMapping("/alterar-senha")
+	@Operation(summary = "Alterar senha de funcionário", description = "Altera a senha de um Funcionário "
+			+ "logado no sistema")
+	public ResponseEntity<Void> alterarSenhaDeUsuario(@RequestBody @Valid DadosAlterarSenhaDeUsuario dados, HttpSession session) {
+		alteraSenhaDeUsuario.alterar(dados, usuarioAutenticado());
+		session.invalidate();
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary = "Buscar dados de funcionário pelo id", description = "Busca um Funcionário "
+			+ "cadastrado no sistema pelo id")
+	public ResponseEntity<DadosDeFuncionario> buscarFuncionarioPeloId(@PathVariable Long id) {
+		DadosDeFuncionario dadosDeFuncionario = buscaDadosDeFuncionario.buscarPeloId(id, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeFuncionario);
+	}
+
+	@GetMapping("/matricula/{matricula}")
+	@Operation(summary = "Buscar dados de funcionário pela matricula", description = "Busca um Funcionário "
+			+ "cadastrado no sistema pela matricula")
+	public ResponseEntity<DadosDeFuncionario> buscarFuncionarioPelaMatricula(@PathVariable String matricula) {
+		DadosDeFuncionario dadosDeFuncionario = buscaDadosDeFuncionario.buscarPelaMatricuka(matricula, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeFuncionario);
+	}
+
+	@GetMapping
+	@Cacheable("buscarTodosFuncionario")
+	@Operation(summary = "Buscar os dados de todos funcionário", description = "Busca os dados de todos "
+			+ "Funcionários cadastrados no sistema")
+	public ResponseEntity<List<DadosDeFuncionario>> buscarTodosFuncionario() {
+		List<DadosDeFuncionario> dadosDeFuncionarios = buscaDadosDeFuncionario.buscarTodos(usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeFuncionarios);
+	}
+
+	private Usuario usuarioAutenticado() {
+		return autenticacaoDeUsuario.recuperarAutenticado();
+	}
+}
---
+++ src/main/java/br/com/totemAutoatendimento/infraestrutura/web/controller/TotemController.java
@@ -0,0 +1,119 @@
+package br.com.totemAutoatendimento.infraestrutura.web.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.totemAutoatendimento.aplicacao.totem.AlteraSenhaDeTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.BuscaDadosDeTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.CriaTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.EditaTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.RemoveTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.dto.DadosCriarTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.dto.DadosDeTotem;
+import br.com.totemAutoatendimento.aplicacao.totem.dto.DadosEditarTotem;
+import br.com.totemAutoatendimento.aplicacao.usuario.dto.DadosAlterarSenhaDeUsuario;
+import br.com.totemAutoatendimento.dominio.totem.Totem;
+import br.com.totemAutoatendimento.dominio.usuario.Usuario;
+import br.com.totemAutoatendimento.infraestrutura.seguranca.AutenticacaoDeUsuario;
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/totem")
+public class TotemController {
+
+	@Autowired
+	private CriaTotem criaTotem;
+	
+	@Autowired
+	private RemoveTotem removeTotem;
+	
+	@Autowired
+	private EditaTotem editaTotem;
+	
+	@Autowired
+	private BuscaDadosDeTotem buscaDadosDeTotem;
+	
+	@Autowired
+	private AlteraSenhaDeTotem alteraSenhaDeTotem;
+	
+	@Autowired
+	private AutenticacaoDeUsuario autenticacaoDeUsuario;
+	
+	@PostMapping
+	@CacheEvict(value = "buscarTodosTotens", allEntries = true)
+	@Operation(summary = "Criar Totem", description = "Cria um Totem no sistema")
+	public ResponseEntity<Totem> criarTotem(@RequestBody @Valid DadosCriarTotem dados) {
+		Totem totem = criaTotem.criar(dados, usuarioAutenticado());
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(totem.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@CacheEvict(value = "buscarTodosTotens", allEntries = true)
+	@Operation(summary = "Remover Totem", description = "Remove um Totem cadastrado no sistema")
+	public ResponseEntity<Void> removerTotem(@PathVariable Long id) {
+		removeTotem.remover(id, usuarioAutenticado());
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	@CacheEvict(value = "buscarTodosTotens", allEntries = true)
+	@Operation(summary = "Editar Totem", description = "Edita um Totem cadastrado no sistema")
+	public ResponseEntity<DadosDeTotem> editarTotem(@PathVariable Long id, @RequestBody @Valid DadosEditarTotem dados) {
+		DadosDeTotem dadosDeTotem = editaTotem.editar(id, dados, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeTotem);
+	}
+	
+	@PatchMapping("/{id}/alterar-senha")
+	@Operation(summary = "Alterar senha de Totem", description = "Altera senha de um Totem cadastrado no sistema")
+	public ResponseEntity<Void> alterarSenhaDeTotem(@PathVariable Long id, @RequestBody @Valid DadosAlterarSenhaDeUsuario dados) {
+		alteraSenhaDeTotem.alterar(id, dados, usuarioAutenticado());
+		return ResponseEntity.ok().build();
+	}
+	
+	@GetMapping("/{id}")
+	@Operation(summary = "Buscar Totem pelo id", description = "Busca os dados de um Totem cadastrado "
+			+ "no sistema pelo id")
+	public ResponseEntity<DadosDeTotem> buscarTotemPeloId(@PathVariable Long id) {
+		DadosDeTotem dadosDeTotem = buscaDadosDeTotem.buscarPeloId(id, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeTotem);
+	}
+	
+	@GetMapping("/registro/{registro}")
+	@Operation(summary = "Buscar Totem pelo identificador", description = "Busca os dados de um Totem "
+			+ "cadastrado no sistema pelo identificador")
+	public ResponseEntity<DadosDeTotem> buscarTotemPeloIdentificador(@PathVariable String identificador) {
+		DadosDeTotem dadosDeTotem = buscaDadosDeTotem.buscarPeloIdentificador(identificador, usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeTotem);
+	}
+	
+	@GetMapping
+	@Cacheable("buscarTodosTotens")
+	@Operation(summary = "Buscar todos os Totens", description = "Busca os dados de todos os "
+			+ "Totens cadastrado no sistema")
+	public ResponseEntity<List<DadosDeTotem>> buscarTodosTotens() {
+		List<DadosDeTotem> dadosDeTotens = buscaDadosDeTotem.buscarTodos(usuarioAutenticado());
+		return ResponseEntity.ok().body(dadosDeTotens);
+	}
+	
+	private Usuario usuarioAutenticado() {
+		return autenticacaoDeUsuario.recuperarAutenticado();
+	}
+}
